94af34c4ca5a553a33d5ffc5ed2ec327562e4f01
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2025-08-16 23:01:17 +0200

    silly ahh test 1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { ref, h } from 'vue'
+
+export default {
+  setup(props) {
+    const someVar = ref(false)
+    window.someVar = someVar;
+
+    return function () {
+      var arr = [
+        h("div", "ahh 1")
+      ]
+
+      if (someVar.value) {
+        arr.push(h("div", "ahh 2"))
+      }
+
+      arr.push(h("div", { id: "someID" }, "ahh 3"))
+
+      return arr
+    }
+  }
+}
+</script>
